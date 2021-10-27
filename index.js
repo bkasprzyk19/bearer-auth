519c897b36eb98e1e03a49ec2b2d7ab0f57747c9
Brian <bkasprzyk@outlook.com>
2021-10-27 10:08:08 -0700

    demo

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+const server = require('./src/server.js')
+// Start up DB Server
+const { db } = require('./src/auth/models/index.js');
+db.sync()
+  .then(() => {
+
+    // Start the web server
+    server.start(process.env.PORT);
+  });
+
